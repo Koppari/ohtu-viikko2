f0e381eb2b1fce84671a0c347abcebe01c12e6d7
Koppari <robin.stachon@helsinki.fi>
2018-11-08 09:13:23 +0200

    t13

--- tehtava9-13/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
+++ tehtava9-13/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
@@ -1,8 +1,9 @@
-
 package ohtu.verkkokauppa;
 
 import java.util.ArrayList;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Kirjanpito implements KirjanpitoInterface {
 
     public static ArrayList<String> tapahtumat;
